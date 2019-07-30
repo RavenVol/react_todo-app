cee2fcc0010b4a09cfcc7d919707b49328512222
RavenVol <r.volianiuk@gmail.com>
2019-07-30 17:19:55 +0300

    Updates

--- precache-manifest.f23beb8d32671773aed0d42423fb4366.js
+++ precache-manifest.f23beb8d32671773aed0d42423fb4366.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "882f28ebdbbf813f9ae8110ee408775a",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "9d56e2c78e20c4ea70a4",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "0e2028188eb1c47e0c8c",
+    "url": "/react_todo-app/static/js/2.a5d31feb.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "9d56e2c78e20c4ea70a4",
+    "url": "/react_todo-app/static/js/main.9295d10e.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
